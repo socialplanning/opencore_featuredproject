4a4bd4c8e838d696848708baf9bd994963da25dd
Ethan Jucovy <ethan.jucovy+github@gmail.com>
2010-06-09 13:14:47 +0000

    fix numerous silly errors. should work now. but i'm thinking this would be better stored on the filesystem than a BTree on site root. small amount of content, infrequent writes almost certainly not in parallel, much easier to keep track of in files..
    git-svn-id: https://svn.socialplanning.org/svn/opencore/plugins/opencore_featuredproject/trunk@26950 40f73b84-6177-4a8e-8c61-7eee4b1974ed

--- opencore_featuredproject/__init__.py
+++ opencore_featuredproject/__init__.py
@@ -1,4 +1,4 @@
-from BTrees.IOBTree import IOBTree
+from BTrees.OOBTree import OOBTree
 from Products.CMFCore.utils import getToolByName
 from datetime import datetime
 from zope.app.annotation.interfaces import IAnnotations
@@ -11,18 +11,18 @@ def get_featured_project_structure():
     annot = IAnnotations(portal)
     featured_structure = annot.get('opencore.feature-project', None)
     if featured_structure is None:
-        featured_structure = IOBTree()
+        featured_structure = OOBTree()
         annot['opencore.feature-project'] = featured_structure
     return featured_structure
 
 def feature_project(project_id, description):
     """this stores the project_id in an annotation on the portal"""
     featured_structure = get_featured_project_structure()
-    featured_structure[project_id] = PersistentDict(
-        project_id=project_id,
-        timestamp=datetime.now(),
-        description=description,
-        )
+    data = PersistentDict()
+    data['project_id'] = project_id
+    data['timestamp'] = datetime.now()
+    data['description'] = description
+    featured_structure[project_id] = data
 
 def get_featured_project_metadata():
     """get some metadata latest featured project, or None"""
--- opencore_featuredproject/browser.py
+++ opencore_featuredproject/browser.py
@@ -7,25 +7,26 @@ from opencore_featuredproject import get_featured_project_metadata
 class FeatureProjectView(BaseView):
     """handle posting to feature a new project"""
 
-    def response(self):
-        return self.request.response.redirect(portal_url + '/projects')
+    def respond(self):
+        portal_url = getToolByName(self.context, 'portal_url')()
+        self.redirect(portal_url + '/projects')
 
-    def __call__(self):
+    def process(self):
         if self.request.environ['REQUEST_METHOD'] != 'POST':
-            return self.render()
+            return
 
         portal_url = getToolByName(self.context, 'portal_url')()
 
         project_id = self.request.form.get('project_id', None)
         if project_id is None:
-            return self.response()
+            return self.respond()
 
         portal = getToolByName(
             self.context, 'portal_url').getPortalObject()
         try:
             project = portal['projects'][project_id]
         except KeyError:
-            return self.response()
+            return self.respond()
 
         desc = self.request.form.get('description', '')
         if desc == '':
@@ -36,7 +37,7 @@ class FeatureProjectView(BaseView):
         self.add_status_message(u'Project <a href="%s">%s</a> featured' %
                                 (portal_url + '/projects/' + project_id,
                                  project_id))
-        return self.response()
+        return self.respond()
 
 class LatestFeaturedProjectView(BrowserView):
     """handle querying for the latest featured project"""
